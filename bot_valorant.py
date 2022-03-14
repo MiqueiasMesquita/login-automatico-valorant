ab1cc5d512a0450f4b2d1c742547335fc2e82dd9
MiqueiasMesquita <82685179+MiqueiasMesquita@users.noreply.github.com>
2022-03-14 17:12:08 -0300

    Create bot_valorant.py

---
+++ bot_valorant.py
@@ -0,0 +1,12 @@
+import pyautogui
+pyautogui.PAUSE = 10
+
+pyautogui.press("win")
+pyautogui.write("VALORANT")
+pyautogui.press("enter")
+pyautogui.click(x=156, y=225)
+pyautogui.write("SEU_NICK")
+pyautogui.click(x=169, y=283)
+pyautogui.write("SUA_SENHA")
+pyautogui.click(x=211, y=473)
+pyautogui.click(x=209, y=688)
